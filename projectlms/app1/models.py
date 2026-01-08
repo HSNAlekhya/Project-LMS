7c9141fd966f0f52b5d5fb70c6da502938b80fed
HANUMA SREE NAGA ALEKHYA GORTHI <ghsnalekhya@gmail.com>
2026-01-08 19:00:24 +0530

    update models.py

--- projectlms/app1/models.py
+++ projectlms/app1/models.py
@@ -7,25 +7,25 @@ class Student(models.Model):
     grade = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.user.username
+        return self.full_name
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author}"
 
 
-class IssueBook(models.Model):
+class IssuedBook(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     issue_date = models.DateField(auto_now_add=True)
-    return_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.book.title} → {self.student.user.username}"
+        return f"{self.book.title} is issued to {self.student.full_name}"
 
 # Create your models here.
+
