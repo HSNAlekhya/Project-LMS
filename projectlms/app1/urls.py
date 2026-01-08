73d6e179e4f44645041a9de65372e593c55d19a2
HANUMA SREE NAGA ALEKHYA GORTHI <ghsnalekhya@gmail.com>
2026-01-08 19:02:07 +0530

    update urls.py

--- projectlms/app1/urls.py
+++ projectlms/app1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from .views import issue_books
 from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('books/', views.books, name='books'),
-    path('issue/', views.issue_book, name='issue_book'),
+    path('issue-books/', views.issue_books, name='issue_books'),
     path('logout/', views.logout_view, name='logout'),
 ]
