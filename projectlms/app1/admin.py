cd1f3b3ca5c2b60ceea44f1fc581bd110fab2942
HANUMA SREE NAGA ALEKHYA GORTHI <ghsnalekhya@gmail.com>
2026-01-08 18:54:40 +0530

    update it

--- projectlms/app1/admin.py
+++ projectlms/app1/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Student, Book, IssueBook
+from .models import Student, Book, IssuedBook
 
 admin.site.register(Student)
 admin.site.register(Book)
-admin.site.register(IssueBook)
+admin.site.register(IssuedBook)
 # Register your models here.
+
